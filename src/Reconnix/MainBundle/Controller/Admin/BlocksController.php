911510df52242bda52e3875cc1643e7c123931dc
Alex Eves <aleves05@gmail.com>
2014-02-27 16:46:11 +0000

    Finished Block adding function and form validation

--- src/Reconnix/MainBundle/Controller/Admin/BlocksController.php
+++ src/Reconnix/MainBundle/Controller/Admin/BlocksController.php
@@ -4,6 +4,7 @@ namespace Reconnix\MainBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Reconnix\MainBundle\Entity\Block;
 
 class BlocksController extends Controller
@@ -16,28 +17,37 @@ class BlocksController extends Controller
     public function addAction(Request $request){
         // create an empty object to store the submitted data
         $block = new Block();
-
-        // create the User form
-        $form = $this->createFormBuilder($block)
-            ->add('name', 'text')
-            ->add('content', 'textarea', array('attr' => array('cols' => '100', 'rows' => '5')))
-            ->add('save', 'submit')
-            ->getForm();
-
+        // create Block form and return a handle to it
+        $form = self::buildBlockForm($block);
         // handle form submission
         $form->handleRequest($request);
         if($form->isValid()){
             // valid form submission
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($block);
-            $em->flush();
-
+            self::persistFormData($block);
+            // return to block index
             return $this->redirect($this->generateUrl('reconnix_main_admin_blocks_index'));
         }
 
+        // no submission detected, or invalid submission
         return $this->render('ReconnixMainBundle:Admin/Blocks:admin.blocks.add.html.twig',
             array('form' => $form->createView())
         );
+    }
+
+    private function buildBlockForm(Block $block){
+        $form = $this->createFormBuilder($block)
+            ->add('name', 'text', array('attr' => array('required' => 'true')))
+            ->add('content', 'textarea', array('attr' => array('cols' => '100', 'rows' => '5')))
+            ->add('save', 'submit')
+            ->getForm();
+
+        return $form;
+    }
+
+    private function persistFormData(Block $block){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($block);
+            $em->flush();       
     }
 
     public function editAction($id){
