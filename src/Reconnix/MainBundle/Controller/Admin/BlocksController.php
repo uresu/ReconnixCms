ed34c54c51fe21de11e759cf9dbdcaf7b693ad96
Alex Eves <aleves05@gmail.com>
2014-02-27 15:10:23 +0000

    Add new Block form persisting to DB - no validation

--- src/Reconnix/MainBundle/Controller/Admin/BlocksController.php
+++ src/Reconnix/MainBundle/Controller/Admin/BlocksController.php
@@ -3,6 +3,8 @@
 namespace Reconnix\MainBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Reconnix\MainBundle\Entity\Block;
 
 class BlocksController extends Controller
 {
@@ -11,9 +13,31 @@ class BlocksController extends Controller
         return $this->render('ReconnixMainBundle:Admin/Blocks:admin.blocks.index.html.twig');
     }
 
-    public function addAction(){
+    public function addAction(Request $request){
+        // create an empty object to store the submitted data
+        $block = new Block();
 
-    	return $this->render('ReconnixMainBundle:Admin/Blocks:admin.blocks.add.html.twig');
+        // create the User form
+        $form = $this->createFormBuilder($block)
+            ->add('name', 'text')
+            ->add('content', 'textarea', array('attr' => array('cols' => '100', 'rows' => '5')))
+            ->add('save', 'submit')
+            ->getForm();
+
+        // handle form submission
+        $form->handleRequest($request);
+        if($form->isValid()){
+            // valid form submission
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($block);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('reconnix_main_admin_blocks_index'));
+        }
+
+        return $this->render('ReconnixMainBundle:Admin/Blocks:admin.blocks.add.html.twig',
+            array('form' => $form->createView())
+        );
     }
 
     public function editAction($id){
--- src/Reconnix/MainBundle/Controller/Admin/PagesController.php
+++ src/Reconnix/MainBundle/Controller/Admin/PagesController.php
@@ -3,6 +3,8 @@
 namespace Reconnix\MainBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class PagesController extends Controller
 {
@@ -11,7 +13,7 @@ class PagesController extends Controller
         return $this->render('ReconnixMainBundle:Admin/Pages:admin.pages.index.html.twig');
     }
 
-    public function addAction(){
+    public function addAction(Request $request){
 
     	return $this->render('ReconnixMainBundle:Admin/Pages:admin.pages.add.html.twig');
     }
