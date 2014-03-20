0662a0da36835ac655f0f3062280141fa026bd50
Alex Eves <aleves05@gmail.com>
2014-03-20 13:46:09 +0000

    Cleanup - Removed depinj and wizard bundle

--- src/Reconnix/ContentWizardBundle/ReconnixContentWizardBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Reconnix\ContentWizardBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ReconnixContentWizardBundle extends Bundle
-{
-}
--- src/Reconnix/DepInjBundle/ReconnixDepInjBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Reconnix\DepInjBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ReconnixDepInjBundle extends Bundle
-{
-}
