ccfe69361ce18396172025c8482656af04b04bb9
Alex Eves <aleves05@gmail.com>
2014-03-31 17:30:32 +0100

    Beginning developer doc

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,21 +1,17 @@
 <?php
 
-use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
- * Behat context class.
+ * Features context.
  */
-class FeatureContext implements SnippetAcceptingContext
+class FeatureContext extends MinkContext
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets it's own context object.
-     * You can also pass arbitrary arguments to the context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
 }
